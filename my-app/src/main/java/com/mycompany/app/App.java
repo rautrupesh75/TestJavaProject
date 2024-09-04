4b2cc48d160665eb8f648fec1641073f766f91f8
rautrupesh <rautrupesh@gmail.com>
2024-09-04 17:59:16 +0530

    code coverage

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,13 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        printMessage();
+    }
+
+    public static void printMessage() {
+        System.out.println("Hello World!");
     }
 }
+
